1cbc49c898e35b0b780f039b0e5ea26f95a0646c
Justin Yue <justinyue1643@gmail.com>
2020-10-03 18:32:45 -0700

    decimal128

--- models/business.js
+++ models/business.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+var SchemaTypes = mongoose.Schema.Types;
+
 // define schema of data
 const businessSchema = new mongoose.Schema({
-  lat: mongoose.SchemaTypes.Double,
-  lon: mongoose.SchemaTypes.Double,
+  lat: SchemaTypes.Decimal128,
+  lon: SchemaTypes.Decimal128,
   address: String,
   name: String,
   description: String,
